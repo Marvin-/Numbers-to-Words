a39d4245feee5ca36125ceb109ceb41053bba091
Marvin M <marvin23m@gmail.com>
2015-12-16 02:55:22 +0000

    Numbers to words works to 99

--- number_to_words.rb
+++ number_to_words.rb
@@ -1,5 +1,24 @@
 module NumberToWords
   def to_words(number = self)
+    change = number.to_s.split("")
+
+    numbers = { 1 => "one", 2 => "two", 3 => "three", 4 =>"four", 5 =>"five", 6 =>"six", 7 =>"seven", 8 =>"eight", 9 => "nine" }
+    teen = {11 => "eleven", 12 => "twelve", 13 => "thirteen", 14 => "fourteen", 15 => "fifteen", 16 => "sixteen", 17 => "seventeen", 18 => "eighteen", 19 => "nineteen"}
+    tens = {10 =>"ten", 20 =>"twenty", 30 =>"thirty", 40 =>"fourty", 50 =>"fifty", 60 =>"sixty", 70 =>"seventy", 80 =>"eighty", 90 =>"ninety"}
+    big = [100 =>"hundred", 1000 =>"thousand", 1000000 =>"million"]
+    
+    if change.size < 3    
+      if change.size < 2      
+        numbers[number].capitalize        
+      elsif change[1].to_i == 0
+        tens[change[0].to_i * 10].capitalize
+      elsif change[0].to_i == 1
+        teen[10 + change[1].to_i].capitalize
+      else
+        tens[change[0].to_i * 10].capitalize + " " + numbers[change[1].to_i].capitalize        
+      end              
+    end 
+
   end
 end
 
--- numbers_to_words_spec.rb
+++ numbers_to_words_spec.rb
@@ -4,11 +4,14 @@ require './number_to_words'
 class NumberToWordsTest < MiniTest::Test
   describe "to_words" do
     it "should handle individual numbers" do
-      1.to_words.must_equal("one")
-      2.to_words.must_equal("two")
-      3.to_words.must_equal("three")
-      11.to_words.must_equal("eleven")
-      12.to_words.must_equal("twelve")
+      1.to_words.must_equal("One")
+      2.to_words.must_equal("Two")
+      3.to_words.must_equal("Three")
+      11.to_words.must_equal("Eleven")
+      18.to_words.must_equal("Eighteen")
+      20.to_words.must_equal("Twenty")
+      38.to_words.must_equal("Thirty Eight")
+      56.to_words.must_equal("Fifty Six")
     end
   end
 end
