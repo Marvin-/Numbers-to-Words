d3c4844efa06fa5976478d79630107273d73e5e9
Marvin M <marvin23m@gmail.com>
2015-12-16 16:14:26 +0000

    Converts to 9999

--- number_to_words.rb
+++ number_to_words.rb
@@ -2,24 +2,41 @@ module NumberToWords
   def to_words(number = self)
     change = number.to_s.split("")
 
-    numbers = { 1 => "one", 2 => "two", 3 => "three", 4 =>"four", 5 =>"five", 6 =>"six", 7 =>"seven", 8 =>"eight", 9 => "nine" }
-    teen = {11 => "eleven", 12 => "twelve", 13 => "thirteen", 14 => "fourteen", 15 => "fifteen", 16 => "sixteen", 17 => "seventeen", 18 => "eighteen", 19 => "nineteen"}
-    tens = {10 =>"ten", 20 =>"twenty", 30 =>"thirty", 40 =>"fourty", 50 =>"fifty", 60 =>"sixty", 70 =>"seventy", 80 =>"eighty", 90 =>"ninety"}
-    big = [100 =>"hundred", 1000 =>"thousand", 1000000 =>"million"]
+    numbers = { 1 => "One", 2 => "Two", 3 => "Three", 4 =>"Four", 5 =>"Five", 6 =>"Six", 7 =>"Seven", 8 =>"Eight", 9 => "Nine" }
+    teen = {11 => "Eleven", 12 => "Twelve", 13 => "Thirteen", 14 => "Fourteen", 15 => "Fifteen", 16 => "Sixteen", 17 => "Seventeen", 18 => "Eighteen", 19 => "Nineteen"}
+    tens = {10 =>"Ten", 20 =>"Twenty", 30 =>"Thirty", 40 =>"Fourty", 50 =>"Fifty", 60 =>"Sixty", 70 =>"Seventy", 80 =>"Eighty", 90 =>"Ninety"}
+    big = {100 =>"Hundred", 1000 =>"Thousand", 1000000 =>"Million"}
     
-    if change.size < 3    
-      if change.size < 2      
-        numbers[number].capitalize        
-      elsif change[1].to_i == 0
-        tens[change[0].to_i * 10].capitalize
-      elsif change[0].to_i == 1
-        teen[10 + change[1].to_i].capitalize
-      else
-        tens[change[0].to_i * 10].capitalize + " " + numbers[change[1].to_i].capitalize        
-      end              
-    end 
+    two_zeros = true if change[1].to_i == 0 && change[2].to_i == 0 
+    three_zeros = true if two_zeros && change[3].to_i == 0
 
+    if change.size < 3    #if less than 100
+      if change.size < 2      #if less than 10
+        numbers[number]        
+      elsif change[1].to_i == 0     # if tens
+        tens[change[0].to_i * 10]
+      elsif change[0].to_i == 1     #if teens
+        teen[10 + change[1].to_i]
+      else
+        tens[change[0].to_i * 10] + " " + numbers[change[1].to_i]        
+      end              
+    elsif change.size == 3      #if in hundreds
+      if two_zeros
+        numbers[change[0].to_i] + " " + big[100]
+      else
+        n = change[1].to_i * 10 + change[2].to_i
+        numbers[change[0].to_i] + " " + big[100] + " " + to_words(n)
+      end
+    elsif change.size == 4 #if in thousands
+      if three_zeros
+        numbers[change[0].to_i] + " " + big[1000]
+      else
+        n = change[1].to_i * 100 + change[2].to_i * 10 + change[3].to_i
+        numbers[change[0].to_i] + " " + big[1000] + " " + to_words(n)
+      end
+    end
   end
+
 end
 
 class Fixnum
--- numbers_to_words_spec.rb
+++ numbers_to_words_spec.rb
@@ -12,6 +12,12 @@ class NumberToWordsTest < MiniTest::Test
       20.to_words.must_equal("Twenty")
       38.to_words.must_equal("Thirty Eight")
       56.to_words.must_equal("Fifty Six")
+      100.to_words.must_equal("One Hundred")
+      222.to_words.must_equal("Two Hundred Twenty Two")
+      500.to_words.must_equal("Five Hundred")
+      487.to_words.must_equal("Four Hundred Eighty Seven")
+      1000.to_words.must_equal("One Thousand")
+      5432.to_words.must_equal("Five Thousand Four Hundred Thirty Two")
     end
   end
 end
